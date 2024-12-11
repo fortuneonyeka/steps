f20208b4c5dca6147e6cad320ddf9fb818fc457c
fortune onyeka <ihedfortune@gmail.com>
2024-12-11 13:14:01 +0100

    passing children prop to receive what is between the open and close of a button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({text,style, onclick, content, className}) => {
+const Button = ({text,style, onclick, content, className, children}) => {
   return (
     <div className='buttons'>
-      <button onClick={onclick} style={style}  className={className}>{content || text}</button>
+      <button onClick={onclick} style={style}  className={className}><span>{children}</span>{content || text}</button>
     </div>
   )
 }
